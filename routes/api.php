7f7c747cd70f8572e7a40b6efb6ec1ac9191ef5e
Brigituciutyte <brigituciutyte@Lorenass-MBP.lan>
2023-04-26 11:32:59 +0300

    add routes to application

---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\AdminPostController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CustomerPostController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UploadController;
+use App\Http\Controllers\UserPostController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::resource('/posts', PostController::class);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::resource('/user/posts', UserPostController::class);
+    Route::post('/user/posts/imageUpload', [UploadController::class, 'handleUpload']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+Route::get('/posts/search/{id}', [PostController::class, 'search']);
