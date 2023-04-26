043810a20b6edd631003784fc9ea69fcacf0d0b8
Brigituciutyte <brigituciutyte@Lorenass-MBP.lan>
2023-04-26 10:29:15 +0300

    add Post, Comment models and set relationships

---
+++ app/Models/Post.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'title', 'excerpt', 'body', 'min_to_read', 'image_path'];
+    protected $hidden = ['created_at', 'updated_at'];
+
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
